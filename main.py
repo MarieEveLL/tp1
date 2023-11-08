3b677d5203073cec367d2b32282c0ab36d5831db
MarieEveLL <lamoureux.laprise.me@gmail.com>
2023-11-08 15:54:48 -0500

    3.gutten tag

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import click
 
-__version__= "0.1.0"
+__version__ = "0.1.1"
+
+
+@click.command("guten-tag")
+def version():
+    click.echo("guten tag")
+
 
 @click.command("version")
 def version():
     click.echo(__version__)
 
+
 @click.command("hello")
 def hello():
     click.echo("hello, world!")
+
 
 if __name__ == "__main__":
     hello()
